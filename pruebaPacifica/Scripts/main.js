6697940f953bf578976993ae654f9cabe0a4ca61
Camilo <guzmenta@hotmail.com>
2019-04-05 01:09:46 -0500

    primer commit

---
+++ pruebaPacifica/Scripts/main.js
@@ -0,0 +1,12 @@
+﻿
+var parroquiaActive = 'false';
+$('#parroquiaCheck').click(function () {
+if (parroquiaActive == true) {
+    $('.hidde').css('display', 'none');
+    parroquiaActive = false;
+}
+else{
+    $('.hidde').css('display', 'block');
+    parroquiaActive = true;
+}
+});
